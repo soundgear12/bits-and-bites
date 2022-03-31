c605b9c563499e0ffe5d010d5513d4aaa50d9f92
Antonio <antonio.rodriguez024@gmail.com>
2022-03-30 22:57:41 -0500

    added components, bg and font

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import MenuComponent from './components/MenuComponent';
+import BurgerComponent from './components/BurgerComponent';
+import AppetizersComponent from './components/AppetizersComponent';
+import EntreesComponent from './components/EntreesComponent';
+import BeerComponent from './components/BeerComponent';
+import CocktailsComponent from './components/CocktailsComponent';
+import EventsComponent from './components/EventsComponent';
+import AboutUsComponent from './components/AboutUsComponent';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/home" element={<MenuComponent />} />
+          <Route exact path="/burgers" element={<BurgerComponent />} />
+          <Route exact path="/appetizers" element={<AppetizersComponent />} />
+          <Route exact path="/entrees" element={<EntreesComponent />} />
+          <Route exact path="/beer" element={<BeerComponent />} />
+          <Route exact path="/cocktails" element={<CocktailsComponent />} />
+          <Route exact path="/events" element={<EventsComponent />} />
+          <Route exact path="/aboutus" element={<AboutUsComponent />} />
+        </Routes>
+      
+      </BrowserRouter>
+    )
+  }
 }
-
-export default App;
---
+++ my-app/src/components/BurgerComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class BurgerComponent extends Component {
+  render() {
+    return (
+      <div className='page'>
+        <h1 className='title'>Burgers</h1>
+        <hr className='line-1'></hr>
+      </div>
+    )
+  }
+}
